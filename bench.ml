f7a3af87ca3c4a20625fcca096743df2cad0d21d
Eric Norige <thelema314@gmail.com>
2011-02-10 17:00:08 -0500

    Finish muxing, reorg run to be a function

--- bench.ml
+++ bench.ml
@@ -41,46 +41,49 @@ let () = Arg2.parse args (fun x -> fns := x :: !fns) usage_info descr notes
 
 (*** PARSER MODULES ***)
 
-module type Parser = sig 
-  type t
-  val id : string
-  val new_parser : unit -> t
-  val delete_parser : t -> unit
-  val add_data : t -> string -> unit
-  val get_event_count : unit -> int
-end
+type 'a parser_f = {
+  id : string;
+  new_parser : unit -> 'a;
+  delete_parser : 'a -> unit;
+  add_data : 'a -> string -> unit;
+(*  get_event_count : unit -> int; *)
+}
 
-module Null : Parser = struct
-  type t = unit
-  let id = "null"
-  let new_parser () = ()
-  let delete_parser () = ()
-  let add_data () _ = ()
-  let get_event_count () = 0
-end
-
+let null_p = {
+  id = "null";
+  new_parser = (fun () -> ());
+  delete_parser = (fun () -> ());
+  add_data = (fun () _ -> ());
+(*  get_event_count = (fun () -> 0); *)
+}
+  
 let trace_len = ref (-1.)
-
-module Flow : Parser = struct 
-  type t = string -> unit
-  let id = "sifter"
-  include Prog_parse
-  let new_parser = new_parser "spec.ca" !extr_ca
+  
+let sift_p = {
+  id = "sift";
+  new_parser = Prog_parse.new_parser "spec.ca" !extr_ca;
+  delete_parser = Prog_parse.delete_parser;
+  add_data = Prog_parse.add_data;
+(*  get_event_count = Prog_parse.get_event_count; *)
+}
     
-  let () = at_exit (fun () -> 
-(*    Printf.printf "#Bytes skip()ed: %d  Times this exceeded the current packet: %d bytes asked to skip trans-packet: %d\n" !Ns_parse.inner_skip !Ns_parse.skip_over !Prog_parse.total_skip; 
-    Printf.printf "#Packets skipped entirely: %d Packets skipped partially: %d\n" !Prog_parse.pkt_skip !Prog_parse.pkt_partial_skip;
-    Printf.printf "#Skip missed: %d\n" !Prog_parse.skip_missed; *)
-    let trace_len = !trace_len /. 8. in
-    let parsed = float !Ns_parse.parsed_bytes /. float (1024 * 1024) in
-    Printf.printf "#Trace payload length: %.1f MB Bytes parsed: %.1f Percentage of flow parsed: %2.1f\n" trace_len parsed (100. *. parsed /. trace_len)
-  )
-end
+let () = at_exit (fun () -> 
+    (*    Printf.printf "#Bytes skip()ed: %d  Times this exceeded the current packet: %d bytes asked to skip trans-packet: %d\n" !Ns_parse.inner_skip !Ns_parse.skip_over !Prog_parse.total_skip; 
+	  Printf.printf "#Packets skipped entirely: %d Packets skipped partially: %d\n" !Prog_parse.pkt_skip !Prog_parse.pkt_partial_skip;
+	  Printf.printf "#Skip missed: %d\n" !Prog_parse.skip_missed; *)
+  let trace_len = !trace_len /. 8. in
+  let parsed = float !Ns_parse.parsed_bytes /. float (1024 * 1024) in
+  Printf.printf "#Trace payload length: %.1f MB Bytes parsed: %.1f Percentage of flow parsed: %2.1f\n" trace_len parsed (100. *. parsed /. trace_len)
+)
+  
 
-module Pac : Parser = struct
-  include Anypac
-  let id = "pac"
-end
+let pac_p = {
+  id = "pac";
+  new_parser = Anypac.new_parser;
+  delete_parser = Anypac.delete_parser;
+  add_data = Anypac.add_data;
+(*  get_event_count = Anypac.get_event_count; *)
+}
 
 (* let is_http ((_sip, _dip, sp, dp),_,_) = sp = 80 || dp = 80 *)
 
@@ -103,40 +106,42 @@ let get_vmsize () =
 
 let conc_flows = ref 0
 
-module Run (P: Parser) = struct
+let run pr = 
 
-  let ht = Hashtbl.create 1000
-  let last_time = ref (Sys.time ())
+  let ht = Hashtbl.create 1000 in
 
   let post_round round time =
-    Hashtbl.iter (fun _ p -> P.delete_parser p; decr conc_flows) ht;
+    Hashtbl.iter (fun _ p -> pr.delete_parser p; decr conc_flows) ht;
     Hashtbl.clear ht;
 (*    if (!conc_flows <> 0) then (
       printf "#ERR: conc_flows = %d after cleanup\n" !conc_flows;
       conc_flows := 0
     ); *)
 (*    Gc.compact (); *)
-    if not check_mem_per_packet then printf "%s\t%d\t%4.2f\n%!" P.id round time
+    if not check_mem_per_packet then printf "%s\t%d\t%4.2f\n%!" pr.id round time
+  in
 
   let act_packet (flow, data, fin) =
     let p = 
       try Hashtbl.find ht flow 
       with Not_found -> 
 	if check_mem_per_packet then incr conc_flows;
-	P.new_parser ()	|> tap (Hashtbl.add ht flow) 
+	pr.new_parser () |> tap (Hashtbl.add ht flow) 
     in
-    P.add_data p data;
+    pr.add_data p data;
     if check_mem_per_packet then printf "%d,%s\n" !conc_flows (get_vmsize ());
     if fin then (
       if check_mem_per_packet then decr conc_flows;
-      Hashtbl.remove ht flow; P.delete_parser p;
+      Hashtbl.remove ht flow; pr.delete_parser p;
     )
-	
+  in	
   let act_flow fl = 
-    let p = P.new_parser () in
-    P.add_data p fl;
-    P.delete_parser p
-end
+    let p = pr.new_parser () in
+    pr.add_data p fl;
+    pr.delete_parser p
+  in
+  post_round, act_packet, act_flow
+
 ;;
 
 
@@ -166,27 +171,55 @@ let print_header () =
   printf "parser\tround\ttime\n%!"
 
 (*** PARSER STRUCTURE ***)
-let run_fs_flow = 
-  [
-    `Null, (let module M = Run(Null)  in main_loop M.post_round M.act_flow);
-    `Sift, (let module M = Run(Flow) in main_loop M.post_round M.act_flow);
-    `Pac,  (let module M = Run(Pac) in main_loop M.post_round M.act_flow);
-  ]
-  
-let run_fs_packet = 
-  [
-    `Null, (let module M = Run(Null)  in main_loop M.post_round M.act_packet);
-    `Sift, (let module M = Run(Flow) in main_loop M.post_round M.act_packet);
-    `Pac,  (let module M = Run(Pac) in main_loop M.post_round M.act_packet);
-  ]  
+let get_fs = function
+  | `Null -> run null_p
+  | `Sift -> run sift_p
+  | `Pac -> run pac_p
+
+let act_packet (p,ap,_af) = main_loop p ap
+let act_flow (p,_ap,af) = main_loop p af
+
+let make_flows fns = fns |> Vect.of_enum |> (fun v -> v, Pcap.mbit_sizef v)
+
+let make_packets fns =
+  let flows = fns |> Enum.map Pcap.read_file_as_str |> Ean_std.number_enum in
+  let ret = Enum.empty () in
+  let send_byte_count () = 
+    match Random.int 5 with
+	0 -> 0
+      | 1 -> Random.int 50
+      | 2 -> Random.int 200
+      | 3 -> Random.int 1000
+      | 4 -> 1000 + Random.int 500
+      | _ -> 1500
+  in
+  let split_rand (id,s) = 
+    let c = send_byte_count () in 
+    let h,t = String.head s c, String.tail s c in
+    if t = "" then 
+      (id,h,true), None
+    else
+      (id,h,false), Some (id, t)
+  in
+  let rec loop ifp =
+    if Enum.is_empty ifp then () else (
+      Enum.get flows |> Option.may (Enum.push ifp);
+      let send, keep = ifp |> Enum.map split_rand |> Enum.uncombine in
+      Random.shuffle send |> Array.enum |> Enum.push ret;
+      Enum.filter_map identity keep |> loop
+    )
+  in
+  loop (Enum.empty ());
+  Enum.concat ret |> Vect.of_enum |> (fun v -> v, Pcap.mbit_size v)
+
 
 (*** MAIN ***) 
 let main () = 
   if !fns = [] then failwith "Not enough arguments";
   let fns = List.enum !fns in
-  let main_loops_perf pre_process acts =
-    let main_null = List.assoc `Null acts in
-    let main_others = List.map (fun p -> List.assoc p acts) !parsers in
+  let main_loops_perf pre_process act =
+    let main_null = get_fs `Null |> act in
+    let main_others = List.map (get_fs |- act) !parsers in
     let xs,len = pre_process fns in
     trace_len := len;
     print_header ();
@@ -194,29 +227,20 @@ let main () =
     let b = List.map (fun f -> f !rep_cnt xs) main_others in
     a,b
   in
-  let main_loops_mem pre_process acts =
-    [0.], List.map (fun p -> (List.assoc p acts) !rep_cnt (pre_process fns |>fst)) !parsers
+  let main_loops_mem pre_process act =
+    [0.], List.map (fun p -> (get_fs p |> act) !rep_cnt (pre_process fns |>fst)) !parsers
   in
   let main_loops = if check_mem_per_packet then main_loops_mem else main_loops_perf in
   let tnull,ts = 
     match !pre_assemble, !mux with
       |	true, false ->
-	main_loops Pcap.assemble run_fs_flow 
+	main_loops Pcap.assemble act_flow 
       | false, false ->
-	main_loops Pcap.pre_parse run_fs_packet
+	main_loops Pcap.pre_parse act_packet
       | true, true ->
-	let make_flows fns = 
-	  fns 
-          |> Enum.map Pcap.read_file_as_str
-          |> Vect.of_enum 
-	  |> (fun v -> v, Pcap.mbit_sizef v)
-	in
-	main_loops make_flows run_fs_flow
+	main_loops make_flows act_flow
       | false, true -> 
-	let make_packets = 
-	  assert false
-	in
-	main_loops make_packets run_fs_packet
+	main_loops make_packets act_packet
   in
   let tnull_avg = list_avg tnull in
   let rates = List.map (List.map (fun t -> !trace_len /. (t -. tnull_avg))) ts in
@@ -224,7 +248,7 @@ let main () =
     printf "#%s rates: %a\n" (p_to_string p) (List.print Float.print) rs) !parsers rates;
   (match rates with [_] -> () | [fr;pr] -> printf "#speed ratio: %4.2f\n" (list_avg fr /. list_avg pr) | _ -> printf "#wrong number of rates\n");
   Printf.printf "#completed in %4.2fs, (sift: %d pac: %d ) events\n" 
-    (Sys.time ()) (Flow.get_event_count ()) (Pac.get_event_count ());
+    (Sys.time ()) (Prog_parse.get_event_count ()) (Anypac.get_event_count ());
   ()
 
 let () = main ()
