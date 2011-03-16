6cfb16d8b93c6632bbab3ce2bdf725ebb31b7c1f
Eric Norige <thelema314@gmail.com>
2011-03-16 17:17:39 -0400

    Tweak soap flow generation code

--- genrec.ml
+++ genrec.ml
@@ -1,4 +1,9 @@
 
+let base_child = 1.0
+let base_sib = 0.6
+let drop_child = 0.8
+let drop_sib = 0.8
+
 
 let op = "<array>"
 let cl = "</array>"
@@ -6,11 +11,11 @@ let entries = ref 0
 let rand_value lev = incr entries; "foo" ^ (string_of_int lev) ^ "_" ^ (string_of_int !entries)
 
 let do_rec min_rec i = 
-  let prob = 1. -. (0.1 *. float (i - min_rec)) in
+  let prob = base_child *. (drop_child ** float (i - min_rec)) in
   Random.float 1. < prob
-let do_adj min_rec i = 
-  let i = max (i - min_rec) 0 in
-  Random.float 1. < 0.6 -. (0.1 *. float i)
+let do_adj _min_rec i = 
+(*  let i = max (i - min_rec) 0 in *)
+  Random.float 1. < base_sib *. (drop_sib ** float i)
 
 let maxi = ref 1
 
@@ -18,7 +23,7 @@ let rec output_grp min_rec print i =
   if i > !maxi then maxi := i;
   print op;
   if do_rec min_rec i then output_grp min_rec print (i+1)
-  else print (rand_value i);
+  else incr entries;(*print (rand_value i); *)
   print cl;
   if do_adj min_rec i then output_grp min_rec print i
 
