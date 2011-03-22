92451153f9be760a4955678e57de12cd60c182d7
Eric Norige <thelema314@gmail.com>
2011-03-22 12:14:58 -0400

    Configurable height/width for figures, memory vs. speed graph, cleanup ranges and labels

--- memory.R
+++ memory.R
@@ -4,6 +4,14 @@ d <- read.csv("~/bpac/rundata", sep="\t")
 d1 <- d[d$parser != "null",]
 d1$parser<-factor(d1$parser,levels=c("bpac","sift","upac"),ordered=FALSE)
 
+picheight=3.5
+picwidth=4.0
+
+### MEMORY vs. SPEED
+ggplot() + theme_bw() + xlab("Parsing Speed (gbps)") + ylab("Memory used(B)") + scale_y_log10() + geom_point(data=d1, aes(shape=parser, x=gbps, y=mem), size=1.5) + opts(legend.position=c(0.8,0.7))
+
+ggsave("overall.pdf", height=picheight, width=picwidth); ggsave("overall.eps", height=picheight, width=picwidth); ggsave("overall.png", height=picheight, width=picwidth, dpi=300)
+
 
 ###MEMORY IMPROVEMENT
 rs <- recast(d1[,c("runid", "parser","mem")], runid ~ parser,mean)
@@ -19,22 +27,22 @@ ggplot() + theme_bw() + xlab(NULL) + ylab("Memory Improvement") +
   c("BinPAC/\nSifter","UltraPAC/\nSifter")) + geom_boxplot(aes(x =
   variable,y = value),data=bs)
 
-ggsave("memimp.pdf", height=3, width=3.5)
-ggsave("memimp.eps", height=3, width=3.5)
-ggsave("memimp.png", height=3, width=3.5, dpi=300)
+ggsave("memimp.pdf", height=picheight, width=picwidth)
+ggsave("memimp.eps", height=picheight, width=picwidth)
+ggsave("memimp.png", height=picheight, width=picwidth, dpi=300)
 
 ### MEMORY PER FLOW
 d1$mpf <- d1$mem / d1$flows
 
 ggplot() +
-  scale_y_log10(breaks=c(100,300,1000,3000,10000,30000),labels=c("100B","300B","1KB","3KB","10KB", "30KB")) +
+  scale_y_log10(breaks=c(100,300,1000,3000,10000,30000,100000),labels=c("100B","300B","1KB","3KB","10KB", "30KB", "100KB")) +
   scale_x_discrete(breaks=c("bpac","sift","upac"), labels=c("BinPAC","Sifter", "UltraPAC")) +
   theme_bw() + xlab(NULL) + ylab("Memory per Flow") +
   geom_jitter(aes(x = parser,y = mpf),data=d1)
 
-ggsave("memper.pdf", height=3, width=3.5)
-ggsave("memper.eps", height=3, width=3.5)
-ggsave("memper.png", height=3, width=3.5, dpi=300)
+ggsave("memper.pdf", height=picheight, width=picwidth)
+ggsave("memper.eps", height=picheight, width=picwidth)
+ggsave("memper.png", height=picheight, width=picwidth, dpi=300)
 
 
 ### SPEED IMPROVEMENT
@@ -45,14 +53,14 @@ y2 <- rs2[,c("runid", "bspeedup", "uspeedup")]
 bs2 <- melt.data.frame(y2)
 
 ggplot() + theme_bw() + xlab(NULL) + ylab("Speed Improvement") +
-  scale_y_continuous(breaks=c(1, 5, 10, 15, 20), labels=c("1x","5x", "10x", "15x", "20x")) + 
+  scale_y_continuous(breaks=c(1, 1.3, 1.6, 2, 2.3), labels=c("1x","1.3x", "1.6x", "2x", "2.3x")) + 
   scale_x_discrete(breaks = c("bspeedup","uspeedup"), labels =
   c("Sifter/\nBinPAC","Sifter/\nUltraPAC")) + geom_boxplot(aes(x =
   variable,y = value),data=bs2)
 
-ggsave("bpsimp.pdf", height=3, width=3.5)
-ggsave("bpsimp.eps", height=3, width=3.5)
-ggsave("bpsimp.png", height=3, width=3.5, dpi=300)
+ggsave("bpsimp.pdf", height=picheight, width=picwidth)
+ggsave("bpsimp.eps", height=picheight, width=picwidth)
+ggsave("bpsimp.png", height=picheight, width=picwidth, dpi=300)
 
 ### MEMORY VS. SPEED
 ggplot() + xlab("Memory Required(B)") + ylab("Speed (gbps)") +
@@ -61,9 +69,9 @@ ggplot() + xlab("Memory Required(B)") + ylab("Speed (gbps)") +
 	geom_point(aes(x = mem,y = gbps,shape = parser),data=d1)
 
 
-ggsave("memspeed.pdf", height=3, width=3.5)
-ggsave("memspeed.eps", height=3, width=3.5)
-ggsave("memspeed.png", height=3, width=3.5, dpi=300)
+ggsave("memspeed.pdf", height=picheight, width=picwidth)
+ggsave("memspeed.eps", height=picheight, width=picwidth)
+ggsave("memspeed.png", height=picheight, width=picwidth, dpi=300)
 
 
 ### SPEED AT SOAP TRACES
@@ -81,6 +89,6 @@ ggplot() +
  geom_bar(aes(y = gbps,x = n),data=d2.sub,alpha = 0.37,fun.data = mean_sdl,mult = 1,stat = 'summary') +
  theme_bw()
 
-ggsave("soapbps.pdf", height=3, width=3.5)
-ggsave("soapbps.eps", height=3, width=3.5)
-ggsave("soapbps.png", height=3, width=3.5, dpi=300)
+ggsave("soapbps.pdf", height=picheight, width=picwidth)
+ggsave("soapbps.eps", height=picheight, width=picwidth)
+ggsave("soapbps.png", height=picheight, width=picwidth, dpi=300)
