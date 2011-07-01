9e622bab0f3d59045b570debeeb21b8047d70a13
Eric Norige <thelema314@gmail.com>
2011-07-01 15:45:55 -0400

    Refactor pcap demo to be used in gen_extr

--- demo.ml
+++ demo.ml
@@ -1,10 +1,8 @@
 open Pcap
-open Batteries_uni
+open Batteries
 open Printf
 
 let debug = true
-
-let new_parser = Prog_parse.new_parser "spec.ca" "extr.ca"
 
 let is_printable c = c = '\n' || (Char.code c >= 32 && Char.code c <= 126) 
 let clean_unprintable s = String.map (fun x -> if is_printable x then x else '.') s 
@@ -15,7 +13,7 @@ let print_flow oc (a,b,c,d) = fprintf oc "(%a,%a,%d,%d)" print_ip a print_ip b c
 
 
 let flow_table = Hashtbl.create 1000
-let act_packet (_ts, flow, _seq_no, data, (_syn, _ack, fin)) =
+let flow_lift new_parser (_ts, flow, _seq_no, data, (_syn, _ack, fin)) =
   if String.length data = 0 && not fin then () else
   let p, is_new = 
     try Hashtbl.find flow_table flow, false
@@ -37,32 +35,36 @@ let gen_string ptr len =
   ret
 
 (* callback *)
-let handle_packet (s:string) (h:pcap_pkthdr) (t:string) =
+let handle_packet act_packet (s:string) (h:pcap_pkthdr) (t:string) =
 (*  let proto = try (int_of_char (String.unsafe_get t 23)) with _ -> 0 in *)
   let t = gen_string t h.caplen in
-  let res = Pcap_parser.packet (Int32.of_int h.ts.tv_sec) t |> Pcap_parser.decode_eth in
-  let payload_size = match res with Some (_,_,_,d,_ as p) -> act_packet p; String.length d | None -> -1 in
-  match payload_size with
-    | -1 -> (* ERROR *)
-      printf "Packet data: %d bytes, failed to parse as eth: %S" h.len t;
-    | 0 -> () (* ignore empty packets *)
-    | x -> 
-      printf "Packet data: %d(%d) bytes, events: %d\n%!" h.len x (!Ns_types.matches)
+  Pcap_parser.packet (Int32.of_int h.ts.tv_sec) t 
+  |> Pcap_parser.decode_eth 
+  |> Option.may act_packet
 
-let main () =
+let pcap_capture filter handler =
   let dev = pcap_lookupdev () in
   printf "Opening interface %s ...\n%!" dev;
   let openlive = pcap_open_live dev 16636 1 0 in
-  printf "Filtering only port 80\n%!";
-  let ok,my_ip,mask = pcap_lookupnet dev in
-  if ok <> 0 then failwith "Couldn't lookup net info on dev";
-  let ok,filter = pcap_compile openlive "port 80" 0 my_ip in
-  if ok <> 0 then failwith "Couldn't compile pattern";
-  if pcap_setfilter openlive filter <> 0 then failwith "Couldn't set filter";
+  if filter <> "" then (
+    printf "Filtering only port 80\n%!";
+    let ok,my_ip,mask = pcap_lookupnet dev in
+    if ok <> 0 then failwith "Couldn't lookup net info on dev";
+    let ok,filter = pcap_compile openlive filter 0 my_ip in
+    if ok <> 0 then failwith "Couldn't compile pattern";
+    if pcap_setfilter openlive filter <> 0 then failwith "Couldn't set filter";
+  );
   printf "Handling packets\n%!";
-  pcap_loop openlive (-1) handle_packet "" |> ignore;
-  flush_all ();
+  pcap_loop openlive (-1) handler "" |> ignore;
   pcap_close openlive
-    
 
-let () = main ()
+let pcap_act filter new_parser = 
+  pcap_capture filter (flow_lift new_parser |> handle_packet)
+
+let main () =
+  let new_parser = Prog_parse.new_parser "spec.ca" "extr.ca" in
+  pcap_act "port 80" new_parser;
+  flush_all ()
+
+(*let () = main () *)
+  
