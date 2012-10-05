48e4f4ef08516467c0b6edff981289dbbd7c87d4
Eric Norige <thelema314@gmail.com>
2012-10-04 20:19:47 -0400

    Updates to compile

--- ns_compile.ml
+++ ns_compile.ml
@@ -34,32 +34,59 @@ let rec print_pexp v oc = function
 
 let declare_uint oc v = fprintf oc "  unsigned int %s;\n" v
 
-let declare_vars oc vs =
+let declare_vars oc var_count =
   fprintf oc "struct state {\n";
-  List.print declare_uint ~first:"" ~last:"" ~sep:"" oc vs;
+  for i = 0 to var_count - 1 do
+    declare_uint oc ("v" ^ string_of_int i);
+  done;
   declare_uint oc "base_pos";
   declare_uint oc "pos";
-  fprintf oc "char* flow_data;\n";
+  fprintf oc "  char* flow_data;\n";
   declare_uint oc "fail_drop";
   declare_uint oc "rerun_temp";
+  declare_uint oc "dfa_best_pri";
+  declare_uint oc "dfa_pri";
+  declare_uint oc "dfa_q";
   fprintf oc "};\n"
 
 let print_act oc (var, act) =
-  fprintf oc "st.v%d = %a;\n" var (print_aexp var) act
+  fprintf oc "st.v%d = %a; " var (print_aexp var) act
 
 let print_acts oc acts =
-  List.print print_act ~first:"" ~last:"" ~sep:"\n" oc acts
+  List.print print_act ~first:"" ~last:"" ~sep:"" oc acts
 
 let print_pred oc idx preds =
   let print_expr oc ps = List.print (fun oc (v,pe) -> print_pexp v oc pe) ~first:"(" ~last:")" ~sep:" && " oc ps in
   fprintf oc "bool p%d = %a;\n" idx print_expr preds
 
+open Regex_dfa
 
 let merge_dec (_act1,nt1 as a) (_act2, nt2 as b) = if nt1 > nt2 then a else b
-let dec_ops = {Regex_dfa.dec0 = ([],-1); merge = merge_dec; cmp = compare}
+let dec_ops = {dec0 = ([],-1); merge = merge_dec; cmp = compare}
 
 type dfa =
-    (unit, int array, (int * Ns_types.ParsedPCFG.a_exp) list * int) Regex_dfa.fa
+    (unit, int array, (int * Ns_types.ParsedPCFG.a_exp) list * int) fa
+
+let dfa_type _dfa = "long"
+
+let print_dfa_table oc i dfa =
+  let print_q oc q = Array.print ~first:"" ~last:"" ~sep:"," Int.print oc q.map in
+  fprintf oc "%s dfa%d[] = " (dfa_type dfa) i;
+  Array.print ~first:"{" ~last:"};\n" ~sep:",\n" print_q oc dfa.qs
+
+let print_dfa_q oc qid q =
+  fprintf oc "case %d: " qid;
+  fprintf oc "switch (st.flow_data[st.pos++]) {";
+  Array.iteri (fun i dest -> fprintf oc "case %d: st.dfa_q = %d; break;\n" i dest) q.map;
+  fprintf oc "}\n"
+
+
+let print_dfa oc dfa id =
+(*  fprintf oc "void DFA%d(state st) {" id;
+  fprintf oc "begin%d: switch(dfa_q) {" id;
+  Array.iteri (print_dfa_q oc)  dfa.Regex_dfa.qs;
+  fprintf oc "}  }\n" *)
+  print_dfa_table oc id dfa
 
 let dfa_ht : (dfa, int) Hashtbl.t = Hashtbl.create 20
 
@@ -73,7 +100,7 @@ let dfaid dfa =
 let rules_eval oc = function
   | [{rx=None; act; nt; prio=_}] ->
     let qnext = Option.default (-1) nt in
-    fprintf oc "st.q = CA%d;\n%abreak;\n" qnext print_acts act;
+    fprintf oc " st.q = CA%d; %a" qnext print_acts act;
   | rules ->
     let dfa = List.enum rules
       |> Enum.map Ns_run.make_rx_pair
@@ -82,7 +109,7 @@ let rules_eval oc = function
       |> Nfa.build_dfa ~labels:false dec_ops
       |> Regex_dfa.minimize
       |> Regex_dfa.to_array in
-    fprintf oc "st.q = DFA%d;\nbreak;\n" (dfaid dfa)
+    fprintf oc " st.dfa_q = %d; st.dfa_best_pri = 0; st.q = DFA%d;" dfa.q0.id (dfaid dfa)
 
 (* print a function for a CA nonterminal *)
 let ca_trans oc idx rules =
@@ -95,31 +122,36 @@ let ca_trans oc idx rules =
     (* GENERATE IDX *)
     fprintf oc "switch (idx) {\n";
     for i = 0 to (1 lsl (List.length rules)) - 1 do
-      fprintf oc "case %d:\n" i;
+      fprintf oc "case %d:" i;
       Ns_run.get_comb i rules |> rules_eval oc;
       fprintf oc "break;\n";
     done;
-    fprintf oc "}\n"; (* close switch *)
+    fprintf oc " }\n"; (* close switch *)
   );
   fprintf oc "}\n" (* end function *)
+
+let gen_header oc var_count =
+  declare_vars oc var_count
 
 let main p e =
   let proto = Ns_parse.parse_file_as_spec p
   and extr = Ns_parse.parse_file_as_extraction e in
-  printf "P1\n";
-  let ca, _var_count =
+  let ca, var_count =
     Ns_parse.merge_cas ~proto ~extr
     |> Ns_parse.regularize
     |> Ns_parse.destring extr.start in
-  printf "P2\n";
   let ca = Ns_parse.dechain ca
 (*    |>tap (Printf.printf "Grammar:\n%a\n" Ns_parse.print_reg_ds_ca)*)
     |> Ns_parse.flatten_priorities
 (*    |> Ns_run.optimize_preds *)
   in
 (*  let oc = File.open_out (e ^ ".c") in *)
-  let oc = stdout in
-  Array.iteri (ca_trans oc) ca
+  let buf = IO.output_string () in
+  let oc = stdout in (* change to file when ready *)
+  Array.iteri (ca_trans buf) ca;
+  gen_header oc var_count;
+  Hashtbl.iter (print_dfa oc) dfa_ht;
+  IO.nwrite oc (IO.close_out buf)
 
 let () = main "http.pro" "extr.ca"
 
